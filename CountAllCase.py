22e270605f97568c3f45fa1afb2750d63c99bc20
bydawn <62323870+saebyeokchu@users.noreply.github.com>
2023-04-05 11:42:32 +0900

    Update CountAllCase.py

--- CountAllCase.py
+++ CountAllCase.py
@@ -26,9 +26,11 @@ for operator in operators :
         counter[operator] = 1
     
 operators_sorted = sorted(counter.items(), key=lambda x:x[1], reverse = True)
+
 print("=" * 23)
 print(operators_sorted)
 print("=" * 23)
+
 #많이 나온 순서대로 인덱스 부여
 i = 0
 operators_to_index = {}
@@ -38,27 +40,38 @@ for (operator, frequency) in operators_sorted :
         operators_to_index[operator] = i
         operators[operators==operator] = i
 
-#print(operators)
+print(operators_to_index)
         
 
-#2 ~ 5개의 패턴만 검사
-patterns = []
-for i in range(len(operators)) : 
-    if i == len(operators) - 1 :
-        break
-    pattern = operators[i:i+2]
+#2~5개 조각으로 검사
+last_pattern = []
+last_dict = {}
+last_array = []
+for j in range(2,6) :
+    patterns = []
+    for i in range(len(operators)) : 
+        if i == len(operators) - j - 1 :
+            break
+        pattern = operators[i:i+j]
+        
+        patterns.append(pattern)
     
-    patterns.append(pattern)
+    unique, counts = np.unique(patterns,axis = 0, return_counts=True)
+    temp = []
+    k = 0
+    for u in unique :
+        temp.append(np.array2string(u))
+        last_array.append([np.array2string(u), counts[k]])
+        k += 1
+        
+    
+    unique_dict = dict(zip(temp, counts))
+    last_dict.update(unique_dict)  
+    unique_sorted = sorted(unique_dict.items(), key=lambda x:x[1], reverse=True)
+    
+    last_pattern.append(unique_sorted)
 
-unique, counts = np.unique(patterns,axis = 0, return_counts=True)
-temp = []
+df = pd.DataFrame(last_array, columns=['pattern','count'])
+df = df.sort_values('count', ascending=False)
 
-for u in unique :
-    temp.append(np.array2string(u))
-
-unique_dict = dict(zip(temp, counts))
-size_two_pattern_sorted = sorted(unique_dict.items(), key=lambda x:x[1], reverse=True)
-
-print("=" * 23)
-print(size_two_pattern_sorted)
-print("=" * 23)
+print(df.head(5))
